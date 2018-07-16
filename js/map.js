b36d03d3fe0b306cfb9b9913216ecbe414486663
Elena Markova <webkwondo.com@gmail.com>
2018-07-17 02:13:24 +0300

    Работы по заданию #16

--- js/map.js
+++ js/map.js
@@ -230,15 +230,15 @@ var renderMapCard = function (similarOffer, id) {
   return mapCardElement;
 };
 
+var displayMapCard = function (offerId) {
+  var mapCard = map.insertBefore(renderMapCard(similarOffers[offerId], offerId), mapFiltersContainer);
+  return mapCard;
+};
+
 var closeMapCard = function (mapCard) {
   if (mapCard.parentNode) {
     mapCard.parentNode.removeChild(mapCard);
   }
-};
-
-var displayMapCard = function (offerId) {
-  var mapCard = map.insertBefore(renderMapCard(similarOffers[offerId], offerId), mapFiltersContainer);
-  return mapCard;
 };
 
 var adForm = document.querySelector('.ad-form');
@@ -283,7 +283,6 @@ var getMapPinMainCoords = function () {
 };
 
 var adFormAdressInput = adForm.querySelector('#address');
-adFormAdressInput.readOnly = true;
 adFormAdressInput.value = getMapPinMainCoords();
 
 var addClickHandlerToMapPins = function () {
@@ -295,14 +294,12 @@ var addClickHandlerToMapPins = function () {
   var onMapPinClick = function (evt) {
     if (clickedElement) {
       closeMapCard(mapCardPopup);
-      clickedElement.classList.remove('clicked');
     }
 
     clickedElement = evt.currentTarget;
     offerid = clickedElement.dataset.offerid;
 
     if (!clickedElement.classList.contains('map__pin--main')) {
-      clickedElement.classList.add('clicked');
       mapCardPopup = displayMapCard(offerid);
       mapCardPopupCloseButton = mapCardPopup.querySelector('.popup__close');
       mapCardPopupCloseButton.addEventListener('click', function () {
@@ -328,3 +325,123 @@ var onMapPinMainMouseup = function () {
 };
 
 mapPinMain.addEventListener('mouseup', onMapPinMainMouseup);
+
+var adFormTitleInput = adForm.querySelector('#title');
+
+adFormTitleInput.addEventListener('invalid', function () {
+  if (adFormTitleInput.validity.tooShort) {
+    adFormTitleInput.setCustomValidity('Заголовок объявления должен быть минимум 30 символов');
+  } else if (adFormTitleInput.validity.tooLong) {
+    adFormTitleInput.setCustomValidity('Заголовок объявления не должен превышать 100 символов');
+  } else if (adFormTitleInput.validity.valueMissing) {
+    adFormTitleInput.setCustomValidity('Обязательное поле');
+  } else {
+    adFormTitleInput.setCustomValidity('');
+  }
+});
+
+var adFormTypeSelect = adForm.querySelector('#type');
+var adFormPriceInput = adForm.querySelector('#price');
+var adFormPriceInputMin = adFormPriceInput.min;
+var adFormPriceInputPlaceholder = adFormPriceInput.placeholder;
+
+var setMinPrice = function () {
+  var adFormTypeSelectedValue = adFormTypeSelect.options[adFormTypeSelect.selectedIndex].value;
+
+  switch (adFormTypeSelectedValue) {
+    case 'bungalo':
+      adFormPriceInputMin = 0;
+      break;
+    case 'flat':
+      adFormPriceInputMin = 1000;
+      break;
+    case 'house':
+      adFormPriceInputMin = 5000;
+      break;
+    case 'palace':
+      adFormPriceInputMin = 10000;
+      break;
+  }
+  adFormPriceInputPlaceholder = adFormPriceInputMin;
+  adFormPriceInput.min = adFormPriceInputMin;
+  adFormPriceInput.placeholder = adFormPriceInputPlaceholder;
+};
+
+adFormTypeSelect.addEventListener('change', function () {
+  setMinPrice();
+});
+
+adFormPriceInput.addEventListener('invalid', function () {
+  if (adFormPriceInput.validity.rangeUnderflow) {
+    adFormPriceInput.setCustomValidity('Цена не может быть меньше ' + adFormPriceInputMin + ' руб.');
+  } else if (adFormPriceInput.validity.rangeOverflow) {
+    adFormPriceInput.setCustomValidity('Цена не может превышать 1000000 руб.');
+  } else if (adFormPriceInput.validity.valueMissing) {
+    adFormPriceInput.setCustomValidity('Обязательное поле');
+  } else {
+    adFormPriceInput.setCustomValidity('');
+  }
+});
+
+var adFormCheckinTimeSelect = adForm.querySelector('#timein');
+var adFormCheckoutTimeSelect = adForm.querySelector('#timeout');
+
+var syncAdFormCheckinTimeAndCheckoutTime = function () {
+  var clickedSelect = null;
+
+  var onSelectChange = function (evt) {
+    clickedSelect = evt.currentTarget;
+    if (clickedSelect === adFormCheckinTimeSelect) {
+      adFormCheckoutTimeSelect.value = adFormCheckinTimeSelect.options[adFormCheckinTimeSelect.selectedIndex].value;
+    } else {
+      adFormCheckinTimeSelect.value = adFormCheckoutTimeSelect.options[adFormCheckoutTimeSelect.selectedIndex].value;
+    }
+  };
+
+  adFormCheckinTimeSelect.addEventListener('change', onSelectChange);
+  adFormCheckoutTimeSelect.addEventListener('change', onSelectChange);
+};
+
+syncAdFormCheckinTimeAndCheckoutTime();
+
+var adFormRoomsSelect = adForm.querySelector('#room_number');
+var adFormCapacitySelect = adForm.querySelector('#capacity');
+var adFormCapacitySelectOptions = adFormCapacitySelect.options;
+var adFormCapacitySelectOption0 = adFormCapacitySelect.querySelector('option[value="0"]');
+var adFormCapacitySelectOption1 = adFormCapacitySelect.querySelector('option[value="1"]');
+var adFormCapacitySelectOption2 = adFormCapacitySelect.querySelector('option[value="2"]');
+var adFormCapacitySelectOption3 = adFormCapacitySelect.querySelector('option[value="3"]');
+
+var syncAdFormRoomsAndCapacity = function () {
+  for (var op = 0; op < adFormCapacitySelectOptions.length; op++) {
+    adFormCapacitySelectOptions[op].disabled = false;
+  }
+
+  var adFormRoomsSelectedValue = adFormRoomsSelect.options[adFormRoomsSelect.selectedIndex].value;
+
+  switch (adFormRoomsSelectedValue) {
+    case '1':
+      adFormCapacitySelectOption0.disabled = true;
+      adFormCapacitySelectOption2.disabled = true;
+      adFormCapacitySelectOption3.disabled = true;
+      break;
+    case '2':
+      adFormCapacitySelectOption0.disabled = true;
+      adFormCapacitySelectOption3.disabled = true;
+      break;
+    case '3':
+      adFormCapacitySelectOption0.disabled = true;
+      break;
+    case '100':
+      adFormCapacitySelectOption1.disabled = true;
+      adFormCapacitySelectOption2.disabled = true;
+      adFormCapacitySelectOption3.disabled = true;
+      break;
+  }
+};
+
+syncAdFormRoomsAndCapacity();
+
+adFormRoomsSelect.addEventListener('change', function () {
+  syncAdFormRoomsAndCapacity();
+});
